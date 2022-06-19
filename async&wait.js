e8efc84b024f7823ccc605a07df190b11f0e0c6b
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-19 02:40:15 -0700

    Create async&wait.js

---
+++ async&wait.js
@@ -0,0 +1,21 @@
+// console.log("Async and await");
+
+// async function harry() {
+
+//  console.log("Inside harry function")
+//  let response = await fetch('https://api.github.com/users');
+//  console.log("Before response");
+//  const users = await response.json();
+// console.log("Users are here");
+// return users
+// console.log("After response")
+
+// }
+// console.log("before calling harry")
+// let a =  harry();
+// console.log(a);
+// a.then(res => console.log(res))
+// console.log("Last line harry ")
+
+ 
+
