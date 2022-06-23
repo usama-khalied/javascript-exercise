f69ebb7a8eb26360c35cd772eabff1df74880efd
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-23 17:02:31 +0500

    Create swichCase.js

---
+++ swichCase.js
@@ -0,0 +1,15 @@
+console.log()
+const age = "4";
+let vari = 25;
+if(age!==4) {
+    console.log("Sahi");
+}
+ if(age!==3){
+console.log("Age is not 30")
+} 
+else {
+    console.log("Nh huwa")
+}
+if(vari) {
+    console.log("Vari is defined")
+}
