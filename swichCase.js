412ebbbed43862c39cca39a22a9d05213a0a5fc1
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-26 01:07:12 -0700

    Update swichCase.js

--- swichCase.js
+++ swichCase.js
@@ -1,15 +1,41 @@
 console.log()
-const age = "4";
-let vari = 25;
-if(age!==4) {
-    console.log("Sahi");
-}
- if(age!==3){
-console.log("Age is not 30")
-} 
-else {
-    console.log("Nh huwa")
-}
-if(vari) {
-    console.log("Vari is defined")
+// const age = "4";
+var num = 32;
+const doesDrive = false;
+// let vari = 25;
+// if(age!==4) {
+//     console.log("Sahi");
+// }
+//  if(age!==3){
+// console.log("Age is not 30")
+// } 
+// else {
+//     console.log("Nh huwa")
+// }
+// if(vari) {
+//     console.log("Vari is defined")
+// }
+// if(doesDrive ||  num > 7) {
+//     console.log("Yes it is working")
+// }
+// else {
+//     console.log("IT is not working")
+// }
+// console.log(num == 45? 'Age is 45' : 'Age is nt 45');
+
+switch (num) {
+    case 18:
+        console.log("You are 18")
+        break;
+        case 19:
+            console.log("You are 19")
+            break;
+            case 45:
+                console.log("You are 40s")
+                break;
+                default :
+                    console.log("You are default")
+                    break;  
+    
+        
 }
