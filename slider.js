978380b607b4dc345332586abfc9f336f0052543
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-28 16:59:23 +0500

    Create slider.js

---
+++ slider.js
@@ -0,0 +1,23 @@
+const section = document.querySelector('.col-slider');
+
+function next() {
+let sectionIndex = 0;    
+// section.style.transform="translateX(-200px)"
+for(let i=0;i<5 ; i++)  {
+    const fonseSize = section.style.width;
+    console.log(typeof fonseSize)
+    fonseSize1 = parseInt(fonseSize) / 2;
+    console.log(fonseSize1.parseInt)
+}
+
+}
+
+Qvise E
+
+// const element = document.querySelector('.element')
+
+// const fontSize = element.style.fontSize
+// console.log(fontSize)
+
+// const color = element.style.color
+// console.log(color)
