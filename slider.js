c946893c5dbf38aca15b04b78e865e4634bc1376
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-28 11:47:03 -0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -3,16 +3,21 @@ const section = document.querySelector('.col-slider');
 function next() {
 let sectionIndex = 0;    
 // section.style.transform="translateX(-200px)"
-for(let i=0;i<5 ; i++)  {
-    const fonseSize = section.style.width;
-    console.log(typeof fonseSize)
-    fonseSize1 = parseInt(fonseSize) / 2;
-    console.log(fonseSize1.parseInt)
-}
+for(let i=0;i<3 ; i++)  {
+    if(i < 5) {
+
+        let fonseSize = parseInt(section.style.width);
+        fonseSize = fonseSize / i;
+        console.log(fonseSize);
+        // section.style.transform="translateX(-${fonseSize} + "PX")";
+        section.style.transform="translateX(-200px)"
+    }
+
+} 
 
 }
 
-Qvise E
+
 
 // const element = document.querySelector('.element')
 
