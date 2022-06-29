95e8fb130e08f4ac4a7e50dcd9d29ac72627076b
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-06-29 10:13:52 -0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,28 +1,18 @@
-const section = document.querySelector('.col-slider');
-
-function next() {
+const section = document.querySelector('.col-slider');  
 let sectionIndex = 0;    
-// section.style.transform="translateX(-200px)"
-for(let i=0;i<3 ; i++)  {
-    if(i < 5) {
+let num = 1;
+function next() {
 
-        let fonseSize = parseInt(section.style.width);
-        fonseSize = fonseSize / i;
-        console.log(fonseSize);
-        // section.style.transform="translateX(-${fonseSize} + "PX")";
-        section.style.transform="translateX(-200px)"
-    }
-
-} 
-
+        // let fonseSize = parseInt(section.style.width);
+    
+        if(sectionIndex < 3) {
+        sectionIndex =  sectionIndex+1;
+        console.log(sectionIndex)
+        section.style.transform="translateX(" + (sectionIndex) * -200 + "px)";
+        }
+        else {
+            sectionIndex = 3;
+        }
 }
 
 
-
-// const element = document.querySelector('.element')
-
-// const fontSize = element.style.fontSize
-// console.log(fontSize)
-
-// const color = element.style.color
-// console.log(color)
