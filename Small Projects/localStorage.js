9762ee194c7b6c40cd9952360d25495574a60040
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-07-05 12:04:08 -0700

    Update localStorage.js

--- Small Projects/localStorage.js
+++ Small Projects/localStorage.js
@@ -1,18 +1,31 @@
 let val = document.querySelector('#val');
 let btn = document.querySelector('#btn');
-
-let result = window.localStorage.setItem('name' , "Anus","Hellow")
-let result1 = window.localStorage.getItem('name');
-
+let para = document.querySelector('.para');
+let del = document.querySelector('.del');
 
 
 btn.addEventListener("click" , function(btn){
-// var colors = [];
-colors.push(val.value)
-for(let i = 0; i < colors.length; i++) {
-  localStorage.setItem("my_colors", JSON.stringify(val.value)); //store colors
-    let storedColors = localStorage.getItem("my_colors"); //get them back
-    console.log(storedColors);
+  var id = (new Date().getTime()).toString(36);
+var obj = {
+  Text:val.value,
+  ID:id
 }
+let get = window.localStorage.setItem(obj.ID,obj.Text);
+let set = window.localStorage.getItem(obj);
+console.log(set)  
 });
+window.onload = function() {
+for(var i = 0; i < localStorage.length; i++) {
+  let x = (localStorage.key(i));
+  let =   new Array(localStorage.length)
+  .fill()
+  .map(i => localStorage.key(i));
+  para.innerHTML = localStorage.key(i);
+  // localStorage.obj = JSON.stringify(Array.from(map.entries(i)));
+}
 
+};
+
+del.addEventListener('click', function() {
+  localStorage.removeItem('obj');
+})
