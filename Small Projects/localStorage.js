69f3b54cc96bae2e8201def545bec3b748f41c2f
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-07-05 12:13:33 -0700

    Update localStorage.js

--- Small Projects/localStorage.js
+++ Small Projects/localStorage.js
@@ -2,30 +2,35 @@ let val = document.querySelector('#val');
 let btn = document.querySelector('#btn');
 let para = document.querySelector('.para');
 let del = document.querySelector('.del');
-
-
-btn.addEventListener("click" , function(btn){
-  var id = (new Date().getTime()).toString(36);
+var id = (new Date().getTime()).toString(36);
 var obj = {
   Text:val.value,
   ID:id
 }
+
+btn.addEventListener("click" , function(btn){
+var obj = () => {
+  ID:id
+  Text:val.value
+}
+
 let get = window.localStorage.setItem(obj.ID,obj.Text);
 let set = window.localStorage.getItem(obj);
 console.log(set)  
 });
 window.onload = function() {
 for(var i = 0; i < localStorage.length; i++) {
+  para.innerHTML = localStorage.key(i);
   let x = (localStorage.key(i));
   let =   new Array(localStorage.length)
   .fill()
   .map(i => localStorage.key(i));
-  para.innerHTML = localStorage.key(i);
+  
   // localStorage.obj = JSON.stringify(Array.from(map.entries(i)));
 }
 
 };
 
 del.addEventListener('click', function() {
-  localStorage.removeItem('obj');
+  localStorage.removeItem('l58j4sah');
 })
