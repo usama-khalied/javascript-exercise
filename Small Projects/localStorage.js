f3952843e5e72c891c45755c6668fa50eefffc49
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-07-13 12:21:00 -0700

    Update localStorage.js

--- Small Projects/localStorage.js
+++ Small Projects/localStorage.js
@@ -3,34 +3,35 @@ let btn = document.querySelector('#btn');
 let para = document.querySelector('.para');
 let del = document.querySelector('.del');
 var id = (new Date().getTime()).toString(36);
-var obj = {
-  Text:val.value,
-  ID:id
-}
+// var obj = {
+//   Text:val.value,
+//   ID:id
+// }
 
 btn.addEventListener("click" , function(btn){
-var obj = () => {
-  ID:id
-  Text:val.value
-}
+  var obj = {
+    ID:id,
+    Text:val.value
+  }
 
-let get = window.localStorage.setItem(obj.ID,obj.Text);
-let set = window.localStorage.getItem(obj);
-console.log(set)  
+let get = window.localStorage.setItem(obj.Text,obj.ID);
+let set = window.localStorage.getItem(obj); 
 });
+
+
+
+// fetch method 
 window.onload = function() {
-for(var i = 0; i < localStorage.length; i++) {
-  para.innerHTML = localStorage.key(i);
-  let x = (localStorage.key(i));
-  let =   new Array(localStorage.length)
+  // let x = (localStorage.key(i));
+  let y  =   new Array(localStorage.length)
   .fill()
   .map(i => localStorage.key(i));
-  
-  // localStorage.obj = JSON.stringify(Array.from(map.entries(i)));
-}
+  para.innerHTML = y;
+
 
 };
 
 del.addEventListener('click', function() {
-  localStorage.removeItem('l58j4sah');
+
+  localStorage.removeItem('"my name is khalied"');
 })
