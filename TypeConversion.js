8c46e62c594b1fda6574f5a92251d079fdfa721e
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-08-06 12:34:28 -0700

    Type of Conversion Practice

---
+++ TypeConversion.js
@@ -0,0 +1,42 @@
+// String to Number Practice 
+
+let myStr = "4444";
+myStr = Number(myStr);
+// console.log(Number(myStr), typeof myStr);
+// let newStr = Number(myStr);
+// Number(myStr);
+// console.log(myStr, typeof myStr);
+
+
+// Number to String Practice
+
+let Nm = 4;
+// Nm = String(Nm);
+// console.log(Nm, typeof Nm);
+
+
+// Boolean to Number
+
+let Story = true;
+// Story = Number(Story);
+// console.log(Story,  typeof Story);
+
+
+// Boolean to String
+
+let Story1 = false;
+// Story1 = String(Story1);
+// console.log(Story1, typeof Story1);
+
+
+// Integer to Number
+
+let int = 44.98;
+// int = parseInt(int);
+// console.log(int , typeof int);
+
+
+// Number to Integer
+let Num = 30;
+Num =  parseFloat(Num);
+console.log(Num, typeof Num);
