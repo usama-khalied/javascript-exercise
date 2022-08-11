28e4567e24a179651bbc63d6fcbf102c39e49a75
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-08-11 15:07:04 +0500

    Update localStorage.js

--- Small Projects/localStorage.js
+++ Small Projects/localStorage.js
@@ -11,21 +11,21 @@ btn.addEventListener("click" , function(btn){
   let val = document.querySelector('#val');
   var id = (new Date().getTime()).toString(36);
   window.localStorage.setItem(val.value,id);
-  // window.localStorage.getItem(val.value); 
+  // window.localStorage.getItem(val.value);
   getArr.push(val.value);
 random();
 myFun();
 val.value = " ";
 
 });
-//Mapping  method 
+//Mapping  method
 function random() {
   let displayMenu = getArr.map(function (item , index) {
     return `<p onclick="myFun(this.textContent)" class="item-text">${item}</p>`;
   });
   displayMenu = displayMenu.join("");
   section.innerHTML = displayMenu;
- 
+
 }
 
 
@@ -38,7 +38,7 @@ function random() {
 
 
 
-// onLoad Fetch Method 
+// onLoad Fetch Method
 window.addEventListener("DOMContentLoaded", function () {
 for(let j =0 ; j < window.localStorage.length; j++){
   getArr.push(localStorage.key(j))
@@ -48,13 +48,13 @@ random();
 
   });
 
-  // Delete method 
+  // Delete method
 del.addEventListener("click",function(e){
   window.location.reload(true);
   localStorage.clear();
 });
 
-// One list delete 
+// One list delete
 // deleteOne.addEventListener("click",function(){
 // alert("Geee");
 // })
@@ -63,14 +63,11 @@ function myFun(val) {
        if(val == localStorage.key(t)) {
         let cut = val
         if(window.confirm("Are you sure you want to delete this list ?")) {
-            localStorage.removeItem(cut);       
-                
+            localStorage.removeItem(cut);
+            window.location.reload();
         }
        }
   }
 
 console.log(getArr);
 }
-
-
-
