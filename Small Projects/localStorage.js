5e43657ebeec73a4b42bfc154021895faf69f10a
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-08-13 14:03:58 -0700

    #very_Closed

--- Small Projects/localStorage.js
+++ Small Projects/localStorage.js
@@ -8,10 +8,7 @@ let del = document.querySelector('.del');
 let getArr = []
 
 btn.addEventListener("click" , function(btn){
-<<<<<<< Updated upstream
-=======
-  
->>>>>>> Stashed changes
+
   let val = document.querySelector('#val');
   var id = (new Date().getTime()).toString(36);
   window.localStorage.setItem(val.value,id);
@@ -23,25 +20,31 @@ val.value = " ";
 
 });
 //Mapping  method
+{/* <button onclick="Checking()">Delete</button> */}
 function random() {
   let displayMenu = getArr.map(function (item , index) {
-    return `<p class="item-text">${item}<button onclick="myFun(this.textContent)">Delete</button></p>`;
+
+
+    return `<p onclick="myFun(this.textContent)" class="item-text">${item}<button id="check" onclick="Checking()">Delete</button></p>`;
   });
   displayMenu = displayMenu.join("");
   section.innerHTML = displayMenu;
-
+}
+function Checking(){
+  let parent = document.querySelector('#check').parentElement.textContent;
+  console.log(parent)
 }
 
-// onclick="myFun(this.textContent)
-
-
-
-
-
-
-
-
-
+function myFun(val) {
+  for(let t =0 ; t < window.localStorage.length; t++){
+       if(val == localStorage.key(t)) {
+        let cut = val
+        if(window.confirm("Are you sure you want to delete this list ?")) {
+            localStorage.removeItem(cut);
+            window.location.reload();
+        }
+       }}}
+// onclick="myFun(this.textContent)"
 // onLoad Fetch Method
 window.addEventListener("DOMContentLoaded", function () {
 for(let j =0 ; j < window.localStorage.length; j++){
@@ -62,17 +65,4 @@ del.addEventListener("click",function(e){
 // deleteOne.addEventListener("click",function(){
 // alert("Geee");
 // })
-function myFun(val) {
-  alert("This .value")
-  for(let t =0 ; t < window.localStorage.length; t++){
-       if(val == localStorage.key(t)) {
-        let cut = val
-        if(window.confirm("Are you sure you want to delete this list ?")) {
-            localStorage.removeItem(cut);
-            window.location.reload();
-        }
-       }
-  }
 
-
-}
