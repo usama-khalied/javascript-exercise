5c71a9d887e62ebb20a3f5fc0432020192ec03e3
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-08-15 20:11:56 -0700

    #Concatenation

--- VariableType.js
+++ VariableType.js
@@ -6,4 +6,11 @@
 // console.log(a);
 
 let convert = "4E";
-console.log(parseInt (convert, 20));
+// console.log(parseInt (convert, 20));
+
+
+// Two Integer Plus and passing one string 
+// The value will be calculate number
+// And string as it is print 
+// this is called "concatenation".
+console.log(2+2+"4");
