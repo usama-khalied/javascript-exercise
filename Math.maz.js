1fcd62a5ef5dbc0b085e8b7a1116c86bb8e6663d
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-08-23 16:18:19 +0500

    #Escappe_Character

--- Math.maz.js
+++ Math.maz.js
@@ -9,10 +9,17 @@ let arrr = [{
         id:93,},];
 
 let ee = arrr.reduce((h1,h2) =>  { 
-    console.log(h1,h2)  
+    // console.log(h1,h2)  
     return h1.id < h2.id ? h2 : h1;
 })
 // });
 // console.log(ee)
 // console.log(ee.id);
 // para.innerHTML = ee.id
+
+
+
+
+// Escape Character
+// document.write("I m a \"good\" boy");
+console.log("I am a \"good\" body"); 
