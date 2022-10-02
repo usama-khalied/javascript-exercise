c09bd4cba70c558a60e64890e7e6cb633aa30931
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-10-02 10:38:17 -0700

    #oop#Object&Inheritance

--- Object.js
+++ Object.js
@@ -51,3 +51,55 @@ let  Person2 = {
 
 printFullName.call(Person2,"Karachi")
 printFullName.call(Person,"Lahore")
+
+
+class Student {
+    constructor(name,age,email) {
+this.myname = name;
+this.myage = age;
+this.myemail = email;
+
+    }
+    biodata() {
+        console.log(`my  name${this.myname} and my age ${this.myage} and my email ${this.myemail}`)
+    }
+}
+let createObje = new Student('Usama',30,'usamakhalied@gmail.com');
+// console.log(createObje)
+createObje.__proto__ = new Student('Anus',10,'anuskhalied@gmail.com')
+
+// console.log(func)
+
+// Inheritance 
+class A {
+    constructor(name,age,email) {
+         this.name =  name;
+         this.email = email;
+         this.age = age
+    }
+      biodata() {
+        return `my  name${this.name} and my age ${this.age} and my email ${this.email}`
+    }
+}
+
+
+
+
+
+
+
+
+
+class B extends A {
+constructor( name,email,age,game) {
+    super(name,age,email)
+     this.game = game;
+}
+playBioData() {
+       return `${super.biodata()} and i play ${this.game}`
+    // console.log(`my name is ${this.name} , and my age ${this.age} and my email ${this.email} and i play ${this.game}`)
+}   
+}
+let newcreateObje = new B('Tayyaba',10,'tayyabakhalied@gmail.com',"hockey");
+console.log(newcreateObje.playBioData())
+// console.log(newcreateObje)
